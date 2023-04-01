f5c2ca794cc3b930cbea6bf99167a422cbfe86b7
Thomas Stadler <thomas@thomasst.xyz>
2023-04-01 12:10:09 +0200

    feat: fetch a given list of backend services
    Signed-off-by: Thomas Stadler <thomas@thomasst.xyz>

--- main.go
+++ main.go
@@ -21,7 +21,9 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"os"
+	"strings"
 )
 
 // Declare default environment variables
@@ -32,6 +34,8 @@ var (
 	AppVersion = "0.1.0"
 	// AppCommit is the commit hash of the application
 	AppCommit = "unknown"
+	// AppBackendServices is a comma-separated list of backend services
+	AppBackendServices []string = []string{"localhost:8080"}
 )
 
 // Read environment variables
@@ -45,17 +49,50 @@ func readEnv() {
 	if c := os.Getenv("APP_COMMIT"); c != "" {
 		AppCommit = c
 	}
+	if b := os.Getenv("APP_BACKEND_SERVICES"); b != "" {
+		b := strings.Split(b, ",")
+		AppBackendServices = b
+	}
 }
+
+var client http.Client
+
+var HelloMessage string = fmt.Sprintf(
+	"Hello from the %s service with version %s from commit %s\n",
+	AppName, AppVersion, AppCommit)
 
 func getRoot(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("got / request\n")
+
 	io.WriteString(w, fmt.Sprintf(
-		"Hello! I'm the %s service, serving version %s from commit %s\n",
-		AppName, AppVersion, AppCommit))
+		"This html is served by the %s service! It fetches data from the backend services.\n\n", AppName))
+
+	for _, backend := range AppBackendServices {
+		fmt.Printf("fetching %s\n", backend)
+		url, err := url.Parse(fmt.Sprintf("http://%s/hello", backend))
+		if err != nil {
+			fmt.Println(err)
+		}
+		resp, err := client.Get(url.String())
+		if err != nil {
+			fmt.Printf("error fetching %s : %s\n", url.String(), err)
+		}
+		defer resp.Body.Close()
+		
+		if resp.StatusCode == http.StatusOK {
+			bodyBytes, err := io.ReadAll(resp.Body)
+			if err != nil {
+				fmt.Println(err)
+			}
+			bodyString := string(bodyBytes)
+			io.WriteString(w, bodyString)
+		}
+	}
 }
+
 func getHello(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("got /hello request\n")
-	io.WriteString(w, "Hello, HTTP!\n")
+	io.WriteString(w, HelloMessage)
 }
 
 func main() {
